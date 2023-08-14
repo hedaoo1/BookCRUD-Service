6dc5706abe906bfbba79e89c918938c0ba38864a
hedaoo1 <98249851+hedaoo1@users.noreply.github.com>
2023-08-13 22:48:13 -0400

    Add files via upload
    Creating basic project-structure for BookCRUD-Service

---
+++ BookCRUD-Service/src/test/java/com/example/BookCRUDService/BookCrudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookCRUDService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCrudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
